d7a174f59ebdf05cf698339e47ca734579c47f7b
AtulRajput01 <atulrajput.work@gmail.com>
2024-10-12 00:18:15 +0530

    adeed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+   
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
